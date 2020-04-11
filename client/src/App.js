9dce93d24d538480eb20da208b8dcd268643c962
Alysia Pereira <alysiapereira5@gmail.com>
2020-04-11 10:20:53 -0400

    Styling to the homepage

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Signup from './components/signUp';
 import Signin from './components/signIn';
 import Navbar from './components/dropdown'
+import "./App.css";
 
 function App() {
   return (
-    <div className="home">
-      <h1>COVID 19</h1>
+    <div>
+      <h1 style= {{ color: "white" }}>COVID 19</h1>
       <div>
         <Signin />
       </div>
