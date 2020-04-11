cebf7c820c7cafc8817016bf49fd8ca62f8792b0
Ozymandias <raymondlee@kaffeinatenc.com>
2020-04-11 10:26:07 -0400

    Added boiler-plate passport authentication and REACT components

--- server.js
+++ server.js
@@ -1,23 +1,41 @@
 const express = require("express");
-
+const passport = require("passport");
 const mongoose = require("mongoose");
-const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const bodyParser = require("body-parser")
+const users = require("./routes/api/users");
 
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-// Add routes, both API and view
-app.use(routes);
+
+
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/covidDB");
+// DB Config
+const db = require("./config/keys").mongoURI;
 
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+
+  app.use(passport.initialize());
+  require("./config/passport")(passport);
+
+  app.use("/api/users", users);
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
