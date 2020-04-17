042a6fef66bc71b5f3ffea60879b580f798b4cf0
Alysia Pereira <alysiapereira5@gmail.com>
2020-04-16 22:06:37 -0400

    user

---
+++ client/build/precache-manifest.d0389d50b3d6f8c9f328f7eedeae661f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "827ae9a96e352eb7c516",
+    "url": "/static/js/main.827ae9a9.chunk.js"
+  },
+  {
+    "revision": "d2fb7e3aab9559a09a3b",
+    "url": "/static/js/1.d2fb7e3a.chunk.js"
+  },
+  {
+    "revision": "827ae9a96e352eb7c516",
+    "url": "/static/css/main.3f782531.chunk.css"
+  },
+  {
+    "revision": "d2fb7e3aab9559a09a3b",
+    "url": "/static/css/1.e70100ca.chunk.css"
+  },
+  {
+    "revision": "6bb151aef89a4b375c67f2c2bbc9930e",
+    "url": "/index.html"
+  }
+];
