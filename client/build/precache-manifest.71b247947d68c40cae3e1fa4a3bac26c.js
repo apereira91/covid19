d742d71ef5698a36402aba934a00d77686a3d106
Alysia Pereira <alysiapereira5@gmail.com>
2020-04-16 20:18:08 -0400

    npm run build

---
+++ client/build/precache-manifest.71b247947d68c40cae3e1fa4a3bac26c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "50e8ff51c1f851e6d0f8",
+    "url": "/static/js/main.50e8ff51.chunk.js"
+  },
+  {
+    "revision": "d2fb7e3aab9559a09a3b",
+    "url": "/static/js/1.d2fb7e3a.chunk.js"
+  },
+  {
+    "revision": "50e8ff51c1f851e6d0f8",
+    "url": "/static/css/main.3f782531.chunk.css"
+  },
+  {
+    "revision": "d2fb7e3aab9559a09a3b",
+    "url": "/static/css/1.e70100ca.chunk.css"
+  },
+  {
+    "revision": "d8c46d8f5d3d44cb44a78d1b21a83ccc",
+    "url": "/index.html"
+  }
+];
